d2b8efa03a307ef1c7e232cd53c11908c27574cf
carldelfin <delfin@carldelfin.com>
2017-04-28 18:07:37 +0200

    fixed a comment

--- scripts/table_1.R
+++ scripts/table_1.R
@@ -1,7 +1,8 @@
 # create a dataframe using variables 18 and 4-14
 table1 <- data.frame(describe(data[c(18, 4:14)]))
 
-# remove columns we don't want
+# remove columns we don't want, 
+# and only retain mean, SD, median, min and max
 table1 <- table1[-c(1,2,6,7,10,11,12,13)]
 
 # rename column headings
